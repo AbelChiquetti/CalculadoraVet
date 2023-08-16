f0d600386d11f424f637aeb8504e70ea99ef8a4e
Abel Chiquetti <77760712+AbelGataum@users.noreply.github.com>
2023-08-16 19:50:16 -0300

    Update unidades-internacionais.js

--- unidades-internacionais.js
+++ unidades-internacionais.js
@@ -1,7 +1,29 @@
+function aplicarMascara(input) {
+    var valor = input.value.replace(/[^\d,.]/g, '');
+    input.value = formatarNumero(valor);
+}
+
+function formatarNumero(numero) {
+    numero = numero.replace(/[.]/g, '');
+    var partes = numero.split(',');
+    if (partes.length > 1) {
+        var parteInteira = partes[0].replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+        return parteInteira + ',' + partes[1];
+    } else {
+        return partes[0].replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+    }
+}
+
+function obterValorNumerico(inputId) {
+    var input = document.getElementById(inputId);
+    var valorFormatado = input.value.replace(/[^\d,.]/g, '');
+    return parseFloat(valorFormatado.replace(',', '.'));
+}
+
 function calcUI_calcularDose() {
-    var peso = parseFloat(document.getElementById('calcUI_peso').value);
-    var doseIndicada = parseFloat(document.getElementById('calcUI_dose').value);
-    var concentracaoMedicamento = parseFloat(document.getElementById('calcUI_concentracao').value);
+    var peso = obterValorNumerico('calcUI_peso');
+    var doseIndicada = obterValorNumerico('calcUI_dose');
+    var concentracaoMedicamento = obterValorNumerico('calcUI_concentracao');
     var concentracaoUnidade = document.getElementById('calcUI_concentracaoUnidade').value;
 
     if (isNaN(peso) || isNaN(doseIndicada) || isNaN(concentracaoMedicamento)) {
@@ -9,9 +31,18 @@ function calcUI_calcularDose() {
         document.getElementById('calcUI_sugestoes').innerText = "";
     } else {
         if (concentracaoUnidade === 'UI/mL') {
-            var doseCalculadaMl = (doseIndicada * peso) / concentracaoMedicamento;
-            document.getElementById('calcUI_resultado').innerText = doseCalculadaMl.toFixed(2) + " mL";
-            document.getElementById('calcUI_sugestoes').innerText = "";
+            var concentracaoMl = parseFloat(document.getElementById('calcUI_concentracaoMl').value);
+
+            if (isNaN(concentracaoMl)) {
+                var doseCalculadaUi = (doseIndicada * peso) / concentracaoMedicamento;
+                document.getElementById('calcUI_resultado').innerText = doseCalculadaUi.toFixed(2) + " UI";
+                document.getElementById('calcUI_sugestoes').innerText = "";
+            } else {
+                var doseCalculadaUi = (doseIndicada * peso) / concentracaoMedicamento;
+                var doseCalculadaMl = (doseIndicada * peso) / (concentracaoMedicamento / concentracaoMl);
+                document.getElementById('calcUI_resultado').innerText = doseCalculadaMl.toFixed(2) + " mL (" + doseCalculadaUi.toFixed(2) + " UI)";
+                document.getElementById('calcUI_sugestoes').innerText = "";
+            }
         } else if (concentracaoUnidade === 'UI/mg') {
             var concentracaoMg = parseFloat(document.getElementById('calcUI_concentracaoMg').value);
             if (isNaN(concentracaoMg)) {
@@ -44,6 +75,7 @@ function calcUI_limparCampos() {
     document.getElementById('calcUI_peso').value = '';
     document.getElementById('calcUI_dose').value = '';
     document.getElementById('calcUI_concentracao').value = '';
+    document.getElementById('calcUI_concentracaoMl').value = '';
     document.getElementById('calcUI_concentracaoMg').value = '';
     document.getElementById('calcUI_concentracaoMcg').value = '';
     document.getElementById('calcUI_resultado').innerText = '';
@@ -63,27 +95,37 @@ function calcUI_limparCampos() {
     }
 }
 
-document.getElementById('calcUI_concentracaoUnidade').addEventListener('change', function() {
+// Faz com que a caixa de concentração em mL (UI/mL) seja visível no carregamento da página
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('calcUI_concentracaoUnidade').value = 'UI/mL';
+    document.getElementById('calcUI_concentracaoMlContainer').style.display = 'flex';
+});
+
+document.getElementById('calcUI_concentracaoUnidade').addEventListener('change', function () {
     const concentracaoUnidade = this.value;
     const concentracaoInput = document.getElementById('calcUI_concentracao');
     const concentracaoMgContainer = document.getElementById('calcUI_concentracaoMgContainer');
     const concentracaoMcgContainer = document.getElementById('calcUI_concentracaoMcgContainer');
+    const concentracaoMlContainer = document.getElementById('calcUI_concentracaoMlContainer'); // Nova linha
 
     if (concentracaoUnidade === 'UI/mL') {
-        concentracaoInput.placeholder = 'Insira a concentração em UI/mL';
+        concentracaoInput.placeholder = 'Insira a concentração em UI';
         concentracaoMgContainer.style.display = 'none';
         concentracaoMcgContainer.style.display = 'none';
+        concentracaoMlContainer.style.display = 'flex'; // Nova linha
         document.getElementById('calcUI_concentracaoMg').value = '';
         document.getElementById('calcUI_concentracaoMcg').value = '';
     } else if (concentracaoUnidade === 'UI/mg') {
         concentracaoInput.placeholder = 'Insira a concentração em UI/mg';
         concentracaoMgContainer.style.display = 'flex';
         concentracaoMcgContainer.style.display = 'none';
+        concentracaoMlContainer.style.display = 'none'; // Nova linha
         document.getElementById('calcUI_concentracaoMcg').value = '';
     } else if (concentracaoUnidade === 'UI/mcg') {
         concentracaoInput.placeholder = 'Insira a concentração em UI/mcg';
         concentracaoMgContainer.style.display = 'none';
         concentracaoMcgContainer.style.display = 'flex';
+        concentracaoMlContainer.style.display = 'none'; // Nova linha
         document.getElementById('calcUI_concentracaoMg').value = '';
     }
 });
